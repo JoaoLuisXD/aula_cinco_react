55179fbe94450dfa789c9bce7a8d3922cad043ad
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-06-07 22:36:56 -0300

    Rotas Válidas e Inválidas, acesso por nome e id

--- src/Routes/Paths.jsx
+++ src/Routes/Paths.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Produtos from "../Pages/Produtos";
 import Home from "../Pages/Home";
 import PageLayout from "../Layouts/PageLayout";
+import NotFound from "../Pages/NotFound";
+import Produto from "../Pages/Produto";
 
 const Paths = () => {
     return (
@@ -10,7 +12,10 @@ const Paths = () => {
                 <Route path="/" element={<PageLayout/>}>
                     <Route index element={<Home />} />
                     <Route path="/Produtos" element={<Produtos />} />
+                    <Route path="/Produto/:id/:nome" element={<Produto />} />
+                    
                 </Route>
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
     );
