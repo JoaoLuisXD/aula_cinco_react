a47a19eab035994863178e84924137ebcfccab25
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-06-08 22:49:51 -0300

    update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,43 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+    background-color: mediumvioletred;
+    padding: 0 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-family: Arial, Helvetica, sans-serif;
+    & h1{
+        color: #FFFFFF;
+    }
+    & nav ul{
+        display: flex;
+        gap: 35px;
+        list-style: none;
+        & li a{
+            color: #FFFFFF80;
+            text-decoration: none;
+            font: 15px;
+
+            &:hover, &.active{
+                color: #FFFFFF;
+            }
+        }
+    }
+`;
 
 const Header = () => {
     return (
-        <>
-        <header>
+        <HeaderContainer>
             <h1>Logo</h1>
             <nav>
                 <ul>
-                    <li><Link to={"/"}>Home</Link></li>
-                    <li><Link to={"Produtos"}>Produtos</Link></li>
+                    <li><NavLink to={"/"}>Home</NavLink></li>
+                    <li><NavLink to={"Produtos"}>Produtos</NavLink></li>
                 </ul>
             </nav>
-        </header>
-        </>
+        </HeaderContainer>
     );
 }
  
